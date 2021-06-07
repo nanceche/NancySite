2a232ca59d9c9fd21424a008cd46ec00c1694b89
Your Name <you@example.com>
2021-06-07 19:21:58 +0100

    more sections

---
+++ main.js
@@ -0,0 +1,13 @@
+// function changePage() {
+//   window.location.pathname =
+//     "./Users/nancyechefu/Documents/school-of-code/NancySite/aboutme.html";
+// }
+
+// addEventListener("scroll", changePage());
+
+// window.addEventListener("scroll", function (e) {
+//   //   console.log(e);
+
+//   window.location.pathname =
+//     "file:///Users/nancyechefu/Documents/school-of-code/NancySite/aboutme.html";
+// });
